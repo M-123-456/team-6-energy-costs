fac8712686af69be650b8f6502e8142eaf69d3a6
M-123-456 <mikigerlach2@gmail.com>
2023-02-06 23:10:54 +0100

    function in climate_data and energy_data added

--- data_preparation/climate_data.py
+++ data_preparation/climate_data.py
@@ -1,30 +1,32 @@
 import pandas as pd
 import data_preparator 
 
-# climate data 2020
-df_wind_2020 = data_preparator.clean_df_climate('../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2020_Excel.csv')
+#? no return
+def save_prep_climate_data(wind_url, solar_url, save_url):
+    # climate data 2020
+    df_wind = data_preparator.clean_df_climate(wind_url)
 
-df_solar_2020 = data_preparator.clean_df_climate('../data/raw/energy-charts_Globale_Solarstrahlung_in_Deutschland_im_Jahr_2020_Excel.csv')
+    df_solar = data_preparator.clean_df_climate(solar_url)
 
-# merge wind and sun data
-df_climate_2020 = pd.merge(df_wind_2020, df_solar_2020)
+    # merge wind and sun data
+    df_climate = pd.merge(df_wind, df_solar)
 
-# save as csv in data folder
-df_climate_2020.to_csv(
-    '../data/prepared/df_climate_2020',
-    index=False
+    # save as csv in data folder
+    df_climate.to_csv(
+        save_url,
+        index=False
+    )
+
+# 2020
+save_prep_climate_data(
+    '../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2020_Excel.csv',
+    '../data/raw/energy-charts_Globale_Solarstrahlung_in_Deutschland_im_Jahr_2020_Excel.csv',
+    '../data/prepared/df_climate_2020'
 )
 
-# climate data 2021
-df_wind_2021 = data_preparator.clean_df_climate('../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2021_Excel.csv')
-
-df_solar_2021 = data_preparator.clean_df_climate('../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2021_Excel.csv')
-
-# merge wind and sun data
-df_climate_2021 = pd.merge(df_wind_2021, df_solar_2021)
-
-# save as csv in data folder
-df_climate_2021.to_csv(
-    '../data/prepared/df_climate_2021',
-    index=False
+# 2021
+save_prep_climate_data(
+    '../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2021_Excel.csv',
+    '../data/raw/energy-charts_Globale_Solarstrahlung_in_Deutschland_im_Jahr_2021_Excel.csv',
+    '../data/prepared/df_climate_2021'
 )
--- data_preparation/energy_data.py
+++ data_preparation/energy_data.py
@@ -1,37 +1,30 @@
 import data_preparator 
-import pandas as pd
 
-###  energy data  ###
-# energy data 2020 cleaning and preparation 
-df_energy_2020 = data_preparator.clean_df_energy('../data/raw/energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2020_Excel.csv')
+def save_prep_energy_data(data_url, save_url):
 
-# add date, day of week and time columns
-df_energy_2020 = data_preparator.add_date_week_time(df_energy_2020)
+    # energy data 2020 cleaning and preparation 
+    df_energy = data_preparator.clean_df_energy(data_url)
 
-# add total energy feeding
-df_energy_2020['total_energy_feeding'] = df_energy_2020['nuclear_power'] + df_energy_2020['not_renewable'] + df_energy_2020['renewable']
+    # add date, day of week and time columns
+    df_energy = data_preparator.add_date_week_time(df_energy)
 
+    # add total energy feeding
+    df_energy['total_energy_feeding'] = df_energy['nuclear_power'] +     df_energy['not_renewable'] + df_energy['renewable']
 
-# save as csv in data folder
-df_energy_2020.to_csv(
-    '../data/prepared/df_energy_price_and_feeding_2020', 
-    index=False
+    # save as csv in data folder
+    df_energy.to_csv(
+        save_url,
+        index=False
+    )
+
+# 2020
+save_prep_energy_data(
+    '../data/raw/energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2020_Excel.csv',
+    '../data/prepared/df_energy_price_and_feeding_2020'
 )
 
-# energy data 2021 cleaning and preparation 
-df_energy_2021 = data_preparator.clean_df_energy('../data/raw/energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2021_Excel.csv')
-
-# add date, day of week and time columns
-df_energy_2020 = data_preparator.add_date_week_time(df_energy_2020)
-
-# add date, day of week and time columns
-df_energy_2021 = data_preparator.add_date_week_time(df_energy_2021)
-
-# add total energy feeding
-df_energy_2021['total_energy_feeding'] = df_energy_2021['nuclear_power'] + df_energy_2021['not_renewable'] + df_energy_2021['renewable']
-
-# save as csv in data folder
-df_energy_2021.to_csv(
-    '../data/prepared/df_energy_price_and_feeding_2021',
-    index=False
+# 2021
+save_prep_energy_data(
+    '../data/raw/energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2021_Excel.csv',
+    '../data/prepared/df_energy_price_and_feeding_2021'
 )
