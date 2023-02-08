9bd4e9189bc3bf79eb1282a1cb09e371313915c8
M-123-456 <mikigerlach2@gmail.com>
2023-02-08 21:23:06 +0100

    data name changed

--- data_preparation/combine_energy_climate.py
+++ data_preparation/combine_energy_climate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df_energy_2020 = pd.read_csv('../data/prepared/df_energy_price_and_feeding_2020.csv')
-df_energy_2021 = pd.read_csv('../data/prepared/df_energy_price_and_feeding_2021.csv')
+df_energy_2020 = pd.read_csv('../data/prepared/df_energy_2020.csv')
+df_energy_2021 = pd.read_csv('../data/prepared/df_energy_2021.csv')
 df_climate_2020 = pd.read_csv('../data/prepared/df_climate_2020.csv')
 df_climate_2021 = pd.read_csv('../data/prepared/df_climate_2021.csv')
 
@@ -20,16 +20,16 @@ def combine_energy_climate(energy_data, climate_data, save_url):
    energy_data_per_hour.reset_index(inplace=True)
    df_energy_climate = pd.merge(climate_data, energy_data_per_hour)
 
-   df_energy_climate.to_csv(save_url)
+   df_energy_climate.to_csv(save_url, index=False)
 
 combine_energy_climate(
    df_energy_2020, 
    df_climate_2020, 
-   '../data/prepared/df_energy_climate_2020'
+   '../data/prepared/df_energy_climate_2020.csv'
 )
 
 combine_energy_climate(
    df_energy_2021, 
    df_climate_2021, 
-   '../data/prepared/df_energy_climate_2021'
+   '../data/prepared/df_energy_climate_2021.csv'
 )
