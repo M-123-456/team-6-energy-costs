f36def4f6dfe390009e05aed17996a608d0adbba
M-123-456 <mikigerlach2@gmail.com>
2023-02-10 22:57:47 +0100

    corrected combine_energy_climate.py and added 2019

--- data_preparation/climate_data.py
+++ data_preparation/climate_data.py
@@ -8,19 +8,27 @@ def save_prep_climate_data(wind_url, solar_url, save_url):
 
     df_solar = data_preparator.clean_df_climate(solar_url)
 
-    # datetime
-    df_wind['datetime'] = pd.to_datetime(df_wind['datetime'])
-    df_solar['datetime'] = pd.to_datetime(df_solar['datetime'])
-
-
     # merge wind and sun data
     df_climate = pd.merge(df_wind, df_solar)
+    # datetime
+    df_climate['datetime'] = pd.to_datetime(df_climate['datetime'])
+
+    # add month
+    df_climate['month_year'] = df_climate['datetime'].dt.to_period('M')
+
 
     # save as csv in data folder
     df_climate.to_csv(
         save_url,
         index=False
     )
+
+# 2019
+save_prep_climate_data(
+    '../data/raw/energy-charts_Windgeschwindigkeit_in_Deutschland_im_Jahr_2019_Excel.csv',
+    '../data/raw/energy-charts_Globale_Solarstrahlung_in_Deutschland_im_Jahr_2019_Excel.csv',
+    '../data/prepared/df_climate_2019.csv'
+)
 
 # 2020
 save_prep_climate_data(
